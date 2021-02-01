41be201003c6f954ad6ae2d98b25d58948484257
Kamil Ismailov <49375867+IsmailovKamil@users.noreply.github.com>
2021-02-01 18:09:38 -0500

    Delete EDA(Exploratory Data Analysis) on English Premier League (soccer) directory

--- EDA(Exploratory Data Analysis) on English Premier League (soccer)/Home Team Performance.sql
+++
@@ -1,9 +0,0 @@
-SELECT "HomeTeam",
-	round(avg("FTHG"),2) as "avg_goals",
-	round(avg("HTHG"),2) as "avg_HT_goals",
-	round(avg("FTAG"),2) as "avg_conceded_goals",
-	round(avg("HTAG"),2) as "avg_HT_conceded_goals",
-	sum("FTHG") as sum_goals,
-	round(avg("HS"),2) as "avg_shots",
-	round(avg("HST"),2) as "avg_shots_on_target"
-FROM public."EPL 2018/2019" group by "HomeTeam" order by "avg_goals" desc
